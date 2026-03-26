1d77d9d9470325c4d987b63f28389ad8752d3105
Shivam Mishra <shivam9878522087@gmail.com>
2026-03-27 00:12:01 +0530

    feat: added user model with mongoose schema

---
+++ src/models/user-model.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const userSchema: Schema = new Schema<IUser>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            select: false,
+        },
+    },
+    { timestamps: true },
+);
+
+const User = mongoose.model<IUser>('User', userSchema);
+
+export default User;
